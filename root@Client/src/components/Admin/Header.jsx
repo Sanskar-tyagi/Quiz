7f4bac00a2141cbee162261330403a2a06fdd45c
Sanskar Tyagi <79279348+Sanskar-tyagi@users.noreply.github.com>
2023-10-29 11:25:39 +0000

    ui

--- root@Client/src/components/Admin/Header.jsx
+++ root@Client/src/components/Admin/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div className='flex bg-white'>
+    <div className='flex bg-white py-2 px-4 rounded-b-lg'>
      <h1 className="text-xl text-black fond-semibold">Welcome Admin</h1>
     </div>
   )
